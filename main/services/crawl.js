eda528fb5945cf95a1ad513dae4686ad9a74da92
dinhbinhnguyen0105 <dinhbinhnguyen0105@gmail.com>
2024-12-10 21:28:49 +0700

    from mac mini

--- main/services/crawl.js
+++ main/services/crawl.js
@@ -1,38 +1,60 @@
-// const puppeteer = require("puppeteer-extra");
-// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-
-// (() => {
-//     puppeteer.use(StealthPlugin());
-
-//     puppeteer.launch({
-// headless : false,
-// executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-// userDataDir: "/Users/ndb/Library/Application Support/Google/Chrome/Default",
-//     })
-
-//     .then(browser => browser.newPage())
-//     .then(page => page.goto('https://www.facebook.com'))
-//     .catch(err => console.error(err));
-// })();
-
-
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const randomUseragent = require('random-useragent'); // Random User-Agent Library
 
 puppeteer.use(StealthPlugin());
 
 (async () => {
+    const count = 5;
     puppeteer.launch({
         headless: false,
+        devtools: true,
         executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-        userDataDir: "/Users/ndb/Library/Application Support/Google/Chrome/Default",
+        userDataDir: "/Users/dinhbinh/Workspace/mymanager-v2/bin/browsers/61566863008632",
     })
         .then(async browser => {
-            console.log('Running tests..')
-            const page = await browser.newPage()
-            await page.goto('https://www.facebook.com/groups/211616406116962/?sorting_setting=CHRONOLOGICAL')
-            
-          })
+            console.log('Running tests..');
+            const page = await browser.newPage();
+            await page.goto('https://www.facebook.com/groups/211616406116962/?sorting_setting=CHRONOLOGICAL');
+            return page;
+        })
+        .then(async page => {
+            const feedElm = await page.waitForSelector("div[role='feed']");
+            const articleElm = await feedElm.waitForSelector("div[aria-describedby]");
+            const describedbyValue = await articleElm.evaluate(elm => {
+                const describedbyAttrValue = elm.getAttribute("aria-describedby");
+                const listOfIds = describedbyAttrValue.split(" ");
+                return {
+                    creatorId: elm.getAttribute("aria-labelledby"),
+                    storyId: listOfIds[0],
+                    messageId: listOfIds[1],
+                    imgId: listOfIds[2],
+                    interactionsId: listOfIds[3],
+                    commentCountId: listOfIds[4],
+                };
+            });
+            // console.log(describedbyValue);
+            // await articleElm.evaluate(elm => elm.parentNode.removeChild(elm));
+            // await new Promise(r => setTimeout(r, 1000));
+            return {
+                articleElm,
+                ...describedbyValue,
+            };
+        })
+        .then(async ({ articleElm, creatorId, storyId, messageId, imgId, interactionsId, commentCountId }) => {
+            const creatorElm = await articleElm.$(`[id="${creatorId}"]`);
+            console.log(creatorElm.getProperty("textContent").getValue());
+        })
+        .catch(error => {
+            console.error("Lỗi:", error.message);
+        });
+
+
 
 })();
+
+// aria-describedby // aria-labelledby
+// data-ad-rendering-role="story_message"
+// data-ad-comet-preview="message"
+// await new Promise(r => setTimeout(r, 2000));
+
+
